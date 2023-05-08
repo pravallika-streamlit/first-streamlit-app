4438774c1f93c9a9ea72240cbcc67e8ec9925d87
pravallika-streamlit <132909184+pravallika-streamlit@users.noreply.github.com>
2023-05-08 18:52:42 +0530

    Update streamlit_app.py
    Added few more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Pravallika First Intresting Learnings on APIs')
+streamlit.header('Pravallika Okayish')
+streamlit.title('Pravallika Good Progress')
+streamlit.title('Pravallika Trying her best')
+streamlit.title('Pravallika learnt new concept')
+
+
