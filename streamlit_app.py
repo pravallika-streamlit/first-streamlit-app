cc76aa57210862c770dea99c58d797ab0aa1ad1b
pravallika-streamlit <132909184+pravallika-streamlit@users.noreply.github.com>
2023-05-16 21:15:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,20 @@ try:
       streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
+
+ streamlit.stop()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+
+streamlit.header("The Fruit Load List contains:")
+streamlit.dataframe(my_data_rows)
+
+add_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding jackfruit', add_fruit)
+
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+    
+    
